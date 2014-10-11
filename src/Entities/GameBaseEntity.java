326220d709b6c859707494a06245b32c79a8a9c3
Steven GERARD <steven.gerard.deruelle@gmail.com>
2014-10-10 22:44:12 -0400

    Add isVisible and isAlive attributs with getters and setters.

--- src/Entities/GameBaseEntity.java
+++ src/Entities/GameBaseEntity.java
@@ -11,10 +11,54 @@ public abstract class GameBaseEntity extends Sprite{
     private int angle;
     // Radius of the entity relative to origin
     private int radius;
+    // Allow to know if the entity is alive or not
+    private boolean isAlive;
+    // Allow to know if the entity is displayable or not
+    private boolean isVisible;
 
+    /**
+     * Default constructor of the GameBaseEntity
+     */
     public GameBaseEntity() {
+        super();
         this.setPosition(0,0);
         this.angle = 0;
         this.radius = 0;
+        this.isAlive = true;
+        this.isVisible = false;
+    }
+
+    //  Accessors   //
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getAngle() {
+
+        return angle;
+    }
+
+    public void setAngle(int angle) {
+        this.angle = angle;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean isVisible) {
+        this.isVisible = isVisible;
     }
 }

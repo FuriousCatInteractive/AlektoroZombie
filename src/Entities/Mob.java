ad583cd28e392c746309c3c78059633e649c6d92
Steven GERARD <steven.gerard.deruelle@gmail.com>
2014-10-11 03:27:24 -0400

    Change constrctor of Mob class

--- src/Entities/Mob.java
+++ src/Entities/Mob.java
@@ -1,11 +1,13 @@
 package Entities;
 
+import MoveBehavior.MoveBehavior;
+
 /**
  * Created by steven on 10/10/14.
  */
 public class Mob extends MovableEntity{
-    public Mob(int id) {
-        super();
+    public Mob(int id, MoveBehavior moveStrategy, int maxSpeed) {
+        super(moveStrategy, maxSpeed);
         this.id = id;
     }
 }

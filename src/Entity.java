0bdeba78d4249ecf0d7e6c0b47f2a1ecd92e0d76
coco <coco@localhost.localdomain>
2014-10-10 22:13:49 -0400

    correction chemin meow.ogg

---
+++ src/Entity.java
@@ -0,0 +1,7 @@
+import org.jsfml.graphics.Sprite;
+
+/**
+ * Created by coco on 14-10-10.
+ */
+public class Entity extends Sprite{
+}
---
+++ src/TextureManager.java
@@ -0,0 +1,5 @@
+/**
+ * Created by coco on 14-10-10.
+ */
+public class TextureManager {
+}

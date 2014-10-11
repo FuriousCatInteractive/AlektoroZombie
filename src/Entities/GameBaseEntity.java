142a5ad48f3c06b3ac528abb485537a7c8dbe5bd
Steven GERARD <steven.gerard.deruelle@gmail.com>
2014-10-10 22:28:32 -0400

    Add BaseGameEntity.

---
+++ src/Entities/GameBaseEntity.java
@@ -0,0 +1,20 @@
+package Entities;
+
+import org.jsfml.graphics.Sprite;
+
+/**
+ * Created by steven on 10/10/14.
+ */
+public class GameBaseEntity extends Sprite{
+
+    // Angle of the entity relative to origin
+    private int angle;
+    // Radius of the entity relative to origin
+    private int radius;
+
+    public GameBaseEntity() {
+        this.setPosition(0,0);
+        this.angle = 0;
+        this.radius = 0;
+    }
+}
